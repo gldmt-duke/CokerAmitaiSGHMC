34c1f25e85ae747dd5ee2b4cfe65e7dc00e88a2e
gldmt-duke <gilad.amitai@duke.edu>
2017-05-01 12:16:43 -0400

    package organization

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='sghmc',
       version='0.1',
       description='An implementation of Stochastic Gradient Hamiltonian Monte Carlo by Gilad Amitai and Beau Coker.',
       license='MIT',
-      scripts=['file with all the stuff'],
+      package=['sghmc'],
       install_requires=[
           'numpy',
       ],
